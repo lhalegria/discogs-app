ca44bd19d2a0fbddb001df0eb4738e121818f970
Luiz Alegria <luizh.alegria@gmail.com>
2026-02-25 20:28:34 -0300

    Initialize Android app module (#1)

---
+++ app/src/main/java/com/example/discogsapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.discogsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Discogs App")
+                }
+            }
+        }
+    }
+}
