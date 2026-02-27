a642803aa1e065a5ec918165c873a393a19bb71e
Luiz Alegria <luizh.alegria@gmail.com>
2026-02-27 00:08:20 -0300

    Add Flow-based base ViewModel (#16)

---
+++ app/src/main/java/com/example/discogsapp/viewmodel/flow/Effect.kt
@@ -0,0 +1,16 @@
+package com.example.discogsapp.viewmodel.flow
+
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+
+interface UIEffect
+
+class Effect<Effect : UIEffect> {
+    private val _uiEffect = MutableSharedFlow<Effect>()
+    val uiEffect: SharedFlow<Effect> = _uiEffect.asSharedFlow()
+
+    suspend fun emitEffect(effect: Effect) {
+        _uiEffect.emit(effect)
+    }
+}
