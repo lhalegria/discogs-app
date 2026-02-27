391b7f2d247b8e431d33f3c4062b4bfe32a51177
Luiz Alegria <luizh.alegria@gmail.com>
2026-02-26 23:19:19 -0300

    Remove hardcoded Discogs API credentials

--- data/src/main/java/com/example/discogsapp/data/network/DiscogsAPI.kt
+++ data/src/main/java/com/example/discogsapp/data/network/DiscogsAPI.kt
@@ -11,10 +11,16 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 private const val BASE_URL = "https://api.discogs.com/"
 private const val DEFAULT_USER_AGENT = "DiscogsApp/1.0 +https://github.com/lhalegria/discogs-app"
-private val DEFAULT_AUTHORIZATION = "Discogs key=%s, secret=%s".format(
-    "DyVhOuvuyGAqAtkvupZN",
-    "eTYXEZeXtNaGGQnqFqRmOVFuLrWVyjWB",
-)
+private val DEFAULT_AUTHORIZATION = when {
+    BuildConfig.DISCOGS_API_KEY.isNotBlank() && BuildConfig.DISCOGS_API_SECRET.isNotBlank() -> {
+        "Discogs key=%s, secret=%s".format(
+            BuildConfig.DISCOGS_API_KEY,
+            BuildConfig.DISCOGS_API_SECRET,
+        )
+    }
+
+    else -> null
+}
 
 private val moshi: Moshi = Moshi.Builder()
     .addLast(KotlinJsonAdapterFactory())
@@ -31,11 +37,15 @@ private fun loggingInterceptor(isDebug: Boolean): HttpLoggingInterceptor =
     }
 
 private fun headersInterceptor(): Interceptor = Interceptor { chain ->
-    val request = chain.request()
+    val requestBuilder = chain.request()
         .newBuilder()
-        .addHeader("Authorization", DEFAULT_AUTHORIZATION)
         .addHeader("User-Agent", DEFAULT_USER_AGENT)
-        .build()
+
+    DEFAULT_AUTHORIZATION?.let {
+        requestBuilder.addHeader("Authorization", it)
+    }
+
+    val request = requestBuilder.build()
 
     chain.proceed(request)
 }
