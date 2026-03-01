fdc8fb83f4fe60c0b8392ad4089b2c391134e86d
Luiz Alegria <luizh.alegria@gmail.com>
2026-03-01 18:02:12 -0300

    Make status bar contrast with system theme (#44)
    * Update status bar style to contrast system theme
    
    * Add SideEffect to theme

--- app/src/main/java/com/example/discogsapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/discogsapp/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.discogsapp.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme =
     darkColorScheme(
@@ -44,6 +49,7 @@ fun DiscogsTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
+    val view = LocalView.current
     val colorScheme =
         when {
             dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -59,6 +65,17 @@ fun DiscogsTheme(
                 LightColorScheme
             }
         }
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            if (darkTheme) window.navigationBarColor = colorScheme.background.toArgb()
+            WindowCompat
+                .getInsetsController(window, view)
+                .isAppearanceLightStatusBars = !darkTheme
+        }
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
